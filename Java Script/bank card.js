05fc180c209d0c3c69b7755701625ae1357bb20c
lewis-Ndungu <106951376+lewis-Ndungu@users.noreply.github.com>
2023-06-22 02:14:14 +0300

    Add files via upload

--- Java Script/bank card.js
+++ Java Script/bank card.js
@@ -1,12 +1,8 @@
-/*
-See on github: https://github.com/muhammederdem/credit-card-form
-*/
-
 new Vue({
   el: "#app",
   data() {
     return {
-      currentCardBackground: Math.floor(Math.random()* 25 + 1), // just for fun :D
+      currentCardBackground: Math.floor(Math.random() * 55 + 1),
       cardName: "",
       cardNumber: "",
       cardMonth: "",
@@ -26,7 +22,7 @@ new Vue({
     document.getElementById("cardNumber").focus();
   },
   computed: {
-    getCardType () {
+    getCardType() {
       let number = this.cardNumber;
       let re = new RegExp("^4");
       if (number.match(re) != null) return "visa";
@@ -39,32 +35,37 @@ new Vue({
 
       re = new RegExp("^6011");
       if (number.match(re) != null) return "discover";
-      
-      re = new RegExp('^9792')
-      if (number.match(re) != null) return 'troy'
+
+      re = new RegExp("^9792");
+      if (number.match(re) != null) return "troy";
+
+      re = new RegExp("^(60|62)");
+      if (number.match(re) != null) return "unionpay";
 
       return "visa"; // default type
     },
-		generateCardNumberMask () {
-			return this.getCardType === "amex" ? this.amexCardMask : this.otherCardMask;
+    generateCardNumberMask() {
+      return this.getCardType === "amex"
+        ? this.amexCardMask
+        : this.otherCardMask;
     },
-    minCardMonth () {
+    minCardMonth() {
       if (this.cardYear === this.minCardYear) return new Date().getMonth() + 1;
       return 1;
     }
   },
   watch: {
-    cardYear () {
+    cardYear() {
       if (this.cardMonth < this.minCardMonth) {
         this.cardMonth = "";
       }
     }
   },
   methods: {
-    flipCard (status) {
+    flipCard(status) {
       this.isCardFlipped = status;
     },
-    focusInput (e) {
+    focusInput(e) {
       this.isInputFocused = true;
       let targetRef = e.target.dataset.ref;
       let target = this.$refs[targetRef];
@@ -72,7 +73,7 @@ new Vue({
         width: `${target.offsetWidth}px`,
         height: `${target.offsetHeight}px`,
         transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`
-      }
+      };
     },
     blurInput() {
       let vm = this;
@@ -85,3 +86,50 @@ new Vue({
     }
   }
 });
+  var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+  if (this.isDeleting) { delta /= 2; }
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
